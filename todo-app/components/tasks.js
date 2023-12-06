b8ddb9fc765bba1756cf1d55de90994bd00590de
Kwasi633 <ezrakwasi633@gmail.com>
2023-12-06 18:22:52 +0000

    Fininsh addTask screen

--- todo-app/components/tasks.js
+++ todo-app/components/tasks.js
@@ -1,18 +1,98 @@
-import { Text, View } from 'react-native'
-import React from 'react'
-import { AntDesign } from '@expo/vector-icons';
+import { Text, View, TextInput, TouchableOpacity } from 'react-native'
+import React, {useState} from 'react'
+import {Picker} from '@react-native-picker/picker';
+import { AntDesign, Feather, FontAwesome, MaterialIcons } from '@expo/vector-icons';
 import styles from '../styles/taskStyles';
 
 export default function Tasks() {
+  const [selectedLanguage, setSelectedLanguage] = useState();
+
+
   return (
     <View style={styles.container}>
-        <View style={styles.title}>
+      <View style={styles.title}>
         <AntDesign name="arrowleft" size={40} color="black" />
         <View style={styles.newBox}>
-        <Text style={styles.textSize}>Add New Task</Text>        
+        <Text style={styles.textSize}>Add New Task</Text>
         </View>
-            </View>
+      </View>
+
+      <View style={styles.inputBox}>
+        <TextInput
+         placeholder='Add a Heading'
+         style={styles.firstInput} 
+         />
+
+        <TextInput
+         multiline
+         numberOfLines={4}
+         placeholder='Add a short description'
+         style={styles.secondInput} 
+         />
+         </View>
+
+        <View style={styles.roundColorsBox}>
+
+        <FontAwesome name="circle" size={40} color="#FF1B1B" />
+        <FontAwesome name="circle" size={40} color="#DBFF00" />
+        <FontAwesome name="circle" size={40} color="#000" />
+        <FontAwesome name="circle" size={40} color="#FF1BE8" />
+        <FontAwesome name="circle" size={40} color="#00C31F" />
+        <FontAwesome name="circle" size={40} color="#00FFF0" /> 
+        </View>
+
+        <View style={styles.iconsBox}>
+        
+        <View style={styles.circleBox}>
+          <View style={styles.iconCircle}>
+          <MaterialIcons name="alarm" size={40} color="white"/>
+          </View>
+
+          <View style={styles.iconCircle}>
+          <Feather name="calendar" size={40} color="white"/>
+          </View>
+          </View>
+    
+    <View style={styles.selectorBox}>
+      <Picker
+        selectedValue={selectedLanguage}
+        onValueChange={(itemValue, itemIndex) =>
+          setSelectedLanguage(itemValue)
+        }>
+        <Picker.Item label="Audio" value="audio" />
+        <Picker.Item label="Lecrae - I will find you" value="lecrae" />
+      </Picker>
+</View>
+
+        
     </View>
+
+      <View style={styles.roundNumBox}>
+
+        <Text style={styles.remindText}>Set Reminder</Text>
+          <View style={styles.numCircle}>
+              <Text>1</Text>
+          </View>
+
+          <View style={styles.numCircle}>
+              <Text>2</Text>
+          </View>
+
+          <View style={styles.numCircle}>
+              <Text>5</Text>
+          </View>
+
+          <View style={styles.numCircle}>
+              <Text>10</Text>
+          </View>
+      </View>
+
+    <View style={styles.addBox}>
+    <TouchableOpacity style={styles.add}>
+        <Text style={styles.addText}>Add Task</Text>
+      </TouchableOpacity>
+    </View>
+      
+  </View>
   )
 }
-
--- todo-app/styles/taskStyles.js
+++ todo-app/styles/taskStyles.js
@@ -3,6 +3,99 @@ import { StyleSheet } from "react-native"
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+      
+    },
+
+    roundColorsBox: {
+        height: 50,
+        marginTop: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-evenly'
+    },
+
+    remindText: {
+        fontSize: 25,
+        fontWeight: 'bold'
+    },
+
+    roundNumBox: {
+        alignItems: 'center',
+        height: 50,
+        marginTop: 25,
+        flexDirection: 'row',
+        justifyContent: 'space-evenly'
+    },
+
+    inputBox: {
+        marginBottom: '50',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        marginTop:10,
+        height: 200,
+    },
+
+    circleBox: {
+        width: '60%',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly'
+    },
+
+    selectorBox: {
+        marginRight: 40,
+        borderWidth: 1,
+        borderRadius: 10,
+        width: '40%',
+        height: 40,
+        margin: 10,
+        justifyContent: 'center'
+    },
+
+    iconsBox: {
+        width: '100%',
+        height: 60,
+        marginTop: 25,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+    },
+
+    iconCircle: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 50,
+        height: 60,
+        width: 60,
+        backgroundColor: '#080930'
+    },
+
+    numCircle: {
+        borderWidth: 1.5,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 50,
+        height: 35,
+        width: 35,
+        borderColor: '#080930'
+    },
+
+    firstInput: {
+            fontStyle: 'italic',
+            borderBottomWidth:2,
+            height: 50,
+            width: '90%',
+            padding: 10,
+            marginBottom: 20,
+        },
+
+    secondInput: {
+        fontStyle: 'italic',
+        borderRadius: 11,
+        padding: 10,
+        height: 100,
+        width: '90%',
+        borderWidth: 2,
+        marginBottom: 30,
+        textAlignVertical: 'top'
     },
 
     title: {
@@ -11,7 +104,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         marginTop: 60,
-        justifyContent: 'space-evenly'
+        justifyContent: 'space-evenly',
     },
 
     newBox: {
@@ -22,6 +115,27 @@ const styles = StyleSheet.create({
     textSize: {
         fontSize: 30,
         fontWeight: 'bold'
+    },
+
+    addBox: {
+        marginTop: 30,
+        width: '100%',
+        height: 65,
+        alignItems: 'center',
+    },
+
+    addText: {
+        fontSize: 20,
+        color: '#FFF'
+    },
+
+    add: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#080930',
+        height: 65,
+        width: '50%',
+        borderRadius: 45,
     }
 })
 
