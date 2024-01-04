84196bb63377ccabbd47d13f825571a1ff0fe61f
Danny Young <danielbampoeocran50@gmail.com>
2024-01-04 09:52:59 +0000

    Final task component

--- todo-app/component/task.js
+++ todo-app/component/task.js
@@ -1,29 +1,41 @@
 import { View, Text,  TextInput, TouchableOpacity} from 'react-native';
 import Style from '../styles/taskStyles.js';
 import React, { useState } from 'react';
-import { MaterialIcons } from '@expo/vector-icons';
-import { FontAwesome,Feather} from '@expo/vector-icons';
-import { Picker } from '@react-native-picker/picker';
-import { Dropdown } from 'react-native-element-dropdown';
-import AntDesign from '@expo/vector-icons/AntDesign';
+import {AntDesign, Feather, FontAwesome, MaterialIcons } from '@expo/vector-icons';
+import {Picker} from '@react-native-picker/picker';
+
+
 
 
 
 const Task = () => {
   const [selectedLanguage, setSelectedLanguage] = useState("");
+
+  
 return (
   <View style={Style.container}> 
-    <View style={Style.heading}>
-    <FontAwesome name = "arrow-left"  size={35} />    
-    <Text style={Style.text}>Add New Task</Text>
-    
+   <View style={Style.heading}>
+        <AntDesign name="arrowleft" size={40} color="black" />
+        <View style={Style.newBox}>
+        <Text style={Style.textSize}>Add New Task</Text>
+        </View>
       </View> 
-     <View>
-     <TextInput style={Style.input} placeholder="Add a heading"/>
-     <TextInput style={Style.inputbig} placeholder="Add a short description"/>
-     </View> 
 
-     <View style={{flexDirection:'row', marginLeft:21, justifyContent:'space-between',marginBottom:32,}} >
+      <View style={Style.inputBox}>
+        <TextInput
+         placeholder='Add a Heading'
+         style={Style.firstInput} 
+         />
+
+        <TextInput
+         multiline
+         numberOfLines={4}
+         placeholder='Add a short description'
+         style={Style.secondInput} 
+         />
+         </View>
+
+     <View style={Style.roundColorBox} >
      <FontAwesome name="circle" size={40} color="#FF1B1B" />
      <FontAwesome name="circle" size={40} color="#DBFF00" />
      <FontAwesome name="circle" size={40} color="#000" />
@@ -32,39 +44,55 @@ return (
      <FontAwesome name="circle" size={40} color="#00FFF0" />
     </View>
 
-    <View style={{flexDirection:'row', justifyContent:'space-between'}} >
-     <View style={{flexDirection:'row', marginLeft:21, justifyContent:'space-between',gap:20, marginBottom:30}} >
-     <MaterialIcons name="alarm" size={50} color="black"/>
-     <Feather name="calendar" size={50} color="black" />
-     </View>
-     <View >
-     
-      <Picker 
-      selectedValue={selectedLanguage}
-      onValueChange={(itemValue, itemIndex) =>
-        setSelectedLanguage(itemValue)
-      } style={{borderWidth:3, borderColor:"red", weight:20, height:30}}>
-      <Picker.Item label="Java" value="java" />
-      <Picker.Item label="JavaScript" value="js" />
-    </Picker>
-    </View>
-    </View> 
-
-    <View style={{flexDirection:'row', marginLeft:5, justifyContent:'space-between',marginBottom:32,}} >
-     <Text style={{fontSize:30}}> Set Timer </Text>
-     <FontAwesome name="circle" size={40} color="#DBFF00" />
-     <FontAwesome name="circle" size={40} color="#000" />
-     <FontAwesome name="circle" size={40} color="#00C31F" />
-     <FontAwesome name="circle" size={40} color="#00C31F" />
-    
-    </View>
-
-    <TouchableOpacity >
-          <View style={Style.button}>
-            <Text style={{color: 'white', fontSize: 20}}>Add Task</Text>
+    <View style={Style.iconsBox}>
+        
+        <View style={Style.circleBox}>
+          <View style={Style.iconCircle}>
+          <MaterialIcons name="alarm" size={40} color="white"/>
           </View>
-    </TouchableOpacity>
 
+          <View style={Style.iconCircle}>
+          <Feather name="calendar" size={40} color="white"/>
+          </View>
+          </View>
+    
+    <View style={Style.selectorBox}>
+      <Picker
+        selectedValue={selectedLanguage}
+        onValueChange={(itemValue, itemIndex) =>
+          setSelectedLanguage(itemValue)
+        }>
+        <Picker.Item label="Audio" value="audio" />
+        <Picker.Item label="Lecrae - I will find you" value="lecrae" />
+      </Picker>
+</View>
+</View> 
+
+<View style={Style.roundNumBox}>
+
+<Text style={Style.remindText}>Set Reminder</Text>
+  <View style={Style.numCircle}>
+      <Text>1</Text>
+  </View>
+
+  <View style={Style.numCircle}>
+      <Text>2</Text>
+  </View>
+
+  <View style={Style.numCircle}>
+      <Text>5</Text>
+  </View>
+
+  <View style={Style.numCircle}>
+      <Text>10</Text>
+  </View>
+</View>
+
+<View style={Style.addBox}>
+<TouchableOpacity style={Style.add}>
+<Text style={Style.addText}>Add Task</Text>
+</TouchableOpacity>
+</View>
 
 
     </View>
--- todo-app/styles/taskStyles.js
+++ todo-app/styles/taskStyles.js
@@ -3,55 +3,168 @@ import { StyleSheet } from 'react-native'
 export default Style = StyleSheet.create({
     // your styles here 
     heading: {
-        flexDirection:"row",
-        alignItems: "center",
-        justifyContent: "space-around",
-        gap:50,
-        marginBottom:30,
-       
-       
+        height: 45,
+        width: "100%",
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 60,
+        justifyContent: 'space-evenly',
+                
     },
+    
     container: {
-        marginTop:64,
-        marginHorizontal:24,
-      
+        marginTop:54,
+        marginHorizontal:20,
         
-      
-      
     },
 
-    input: {
+    textSize: {
+        fontSize: 30,
+        fontWeight: 'bold'
+    },
+
+    inputBox: {
+        marginBottom: '50',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        marginTop:10,
+        height: 200,
+    },
+
+    firstInput: {
+        fontStyle: 'italic',
         borderBottomWidth:2,
-        padding:10,
-        marginBottom:30
-        
-
+        height: 50,
+        width: '90%',
+        padding: 10,
+        marginBottom: 20,
     },
 
-    inputbig: {
-        borderWidth:2,
-        padding:15,
-        borderRadius:10,
-        height:130,
-        marginBottom:30
-       
+secondInput: {
+    fontStyle: 'italic',
+    borderRadius: 11,
+    padding: 10,
+    height: 100,
+    width: '90%',
+    borderWidth: 2,
+    marginBottom: 30,
+    textAlignVertical: 'top'
+},
+
+    roundColorBox: {
+        flexDirection:'row', 
+        justifyContent:'space-evenly',
+        marginTop:10,
+        height: 50
     },
 
-    text: {
-       flex:4, 
-       fontSize:30, 
-       
-            },
+    circleBox: {
+        width: '50%',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly'
+    },
 
-    button: {
-       backgroundColor: 'black',
-       height:40, 
-       weight:'50%' ,
-       justifyContent:'center',
-       alignItems: 'center',
-       borderRadius: 100
-       
-            }
+    selectorBox: {
+        marginRight: 40,
+        borderWidth: 1,
+        borderRadius: 10,
+        width: '40%',
+        height: 40,
+        margin: 10,
+        justifyContent: 'center'
+    },
+
+    remindText: {
+        fontSize: 25,
+        fontWeight: 'bold'
+    },
+
+    iconsBox: {
+        width: '100%',
+        height: 60,
+        marginTop: 25,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+    },
+
+    iconCircle: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 50,
+        height: 60,
+        width: 60,
+        backgroundColor: '#080930'
+    },
+
+    numCircle: {
+        borderWidth: 1.5,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 50,
+        height: 35,
+        width: 35,
+        borderColor: '#080930'
+    },
+
+    firstInput: {
+            fontStyle: 'italic',
+            borderBottomWidth:2,
+            height: 50,
+            width: '90%',
+            padding: 10,
+            marginBottom: 20,
+        },
+
+    secondInput: {
+        fontStyle: 'italic',
+        borderRadius: 11,
+        padding: 10,
+        height: 100,
+        width: '90%',
+        borderWidth: 2,
+        marginBottom: 30,
+        textAlignVertical: 'top'
+    },
+
+     newBox: {
+        width: '75%',
+        alignItems: 'center',
+    },
+
+    textSize: {
+        fontSize: 30,
+        fontWeight: 'bold'
+    },
+
+    addBox: {
+        marginTop: 30,
+        width: '100%',
+        height: 65,
+        alignItems: 'center',
+    },
+
+    addText: {
+        fontSize: 20,
+        color: '#FFF'
+    },
+
+    add: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#080930',
+        height: 65,
+        width: '50%',
+        borderRadius: 45,
+    },
+
+    roundNumBox: {
+        alignItems: 'center',
+        height: 50,
+        marginTop: 25,
+        flexDirection: 'row',
+        justifyContent: 'space-evenly'
+    },
 });
 
 
